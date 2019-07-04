34428a167c90d9fb05a19afac7a8a759f8fcb23f
unknown <swest06@dcs.bbk.ac.uk>
2019-07-04 18:52:14 +0100

    got splash screen working

---
+++ app/src/main/java/com/project/siapp/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.project.siapp
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        val background = object : Thread(){
+            override fun run() {
+                try{
+                    Thread.sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
