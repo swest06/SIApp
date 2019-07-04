5de72bc1b8c079ffbcfd73caa99eee5a534ee6ff
unknown <swest06@dcs.bbk.ac.uk>
2019-07-04 15:50:07 +0100

    first commit (animated gradient background works)

---
+++ app/src/main/java/com/project/siapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.project.siapp
+
+import android.graphics.drawable.AnimationDrawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        //Animated gradient background
+        val constraintLayout: ConstraintLayout = findViewById(R.id.layout)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
+    }
+}
---
+++ app/src/main/java/com/project/siapp/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.project.siapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+//class SplashActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        //From Reso Coder
+////        if (FirebaseAuth.getInstance()).currentUser == null){
+////            startActiity<SignInActivity>()
+////        }else{
+////            startActivity<MainActivity>()
+////        }
+////        finish()
+//    }
+//}
