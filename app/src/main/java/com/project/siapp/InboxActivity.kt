bf5c98ee001a4530beaf4324d1bcbab93b76842a
unknown <swest06@dcs.bbk.ac.uk>
2019-07-22 14:42:02 +0100

    add InboxActivity

---
+++ app/src/main/java/com/project/siapp/InboxActivity.kt
@@ -0,0 +1,12 @@
+package com.project.siapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InboxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inbox)
+    }
+}
