fda611ef88475745a5f5e3f1cb252cdea5ac058e
unknown <swest06@dcs.bbk.ac.uk>
2019-07-23 18:07:18 +0100

    add groupie dependency

--- app/src/main/java/com/project/siapp/SearchActivity.kt
+++ app/src/main/java/com/project/siapp/SearchActivity.kt
@@ -2,6 +2,8 @@ package com.project.siapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
@@ -9,5 +11,9 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         supportActionBar?.title = "Search Users"
+
+        //setup adapter. Might need to make a custom adapter class that extends RecyclerView.Adapter<ViewHolder>
+        recycler_view_search.adapter
+        recycler_view_search.layoutManager = LinearLayoutManager(this) //can be added in xml instead
     }
 }
