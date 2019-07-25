021700717c6b82bba45452a2842ff3dc049982c8
unknown <swest06@dcs.bbk.ac.uk>
2019-07-25 15:04:51 +0100

    edit cardview

---
+++ app/src/main/java/com/project/siapp/ProfileSnippet.kt
@@ -0,0 +1,18 @@
+package com.project.siapp
+
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+
+class ProfileSnippet : Item<ViewHolder>() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+
+        //return a layout file R.layout.?
+        return 0
+    }
+}
--- app/src/main/java/com/project/siapp/SearchActivity.kt
+++ app/src/main/java/com/project/siapp/SearchActivity.kt
@@ -3,6 +3,8 @@ package com.project.siapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
@@ -13,7 +15,8 @@ class SearchActivity : AppCompatActivity() {
         supportActionBar?.title = "Search Users"
 
         //setup adapter. Might need to make a custom adapter class that extends RecyclerView.Adapter<ViewHolder>
-        recycler_view_search.adapter
+        val adapter = GroupAdapter<ViewHolder>()
+        recycler_view_search.adapter = adapter
         recycler_view_search.layoutManager = LinearLayoutManager(this) //can be added in xml instead
     }
 }
