4c0bfe8d53c5dc9494c01e8ba5a053b4a4643424
unknown <swest06@dcs.bbk.ac.uk>
2019-08-13 20:32:47 +0100

    fix profile image null pointer exceptions etc

--- app/src/main/java/com/project/siapp/OtherUserProfileActivity.kt
+++ app/src/main/java/com/project/siapp/OtherUserProfileActivity.kt
@@ -2,6 +2,7 @@ package com.project.siapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
@@ -11,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_other_user_profile.*
 class OtherUserProfileActivity : AppCompatActivity() {
     private val messageButton by lazy { findViewById<Button>(R.id.message_button_other_user_profile) }
     private val addToGroupButton by lazy { findViewById<Button>(R.id.add_to_group_button_other_user_profile) }
+    private val TAG = "OtherUserProfileActiv"
 
     //parcel object
     val user by lazy { intent.getParcelableExtra<User>(SearchActivity.USER_KEY) }
@@ -23,6 +25,10 @@ class OtherUserProfileActivity : AppCompatActivity() {
     val userId by lazy { intent.getStringExtra(SearchActivity.USER_ID) }
     val userPhoto by lazy { intent.getStringExtra(SearchActivity.USER_PHOTO) }
     val userAbout by lazy { intent.getStringExtra(SearchActivity.USER_ABOUT) }
+
+    val photoButton by lazy { findViewById<Button>(R.id.photo_other_user_profile) }
+
+
 
     //constants to pass to intents (may just end up using user id)
     companion object{
@@ -42,6 +48,7 @@ class OtherUserProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_user_profile)
         supportActionBar?.title = "Profile $username"
+        photoButton.alpha = 0F
 
         //set profile info
         setFields()
@@ -75,9 +82,13 @@ class OtherUserProfileActivity : AppCompatActivity() {
         val circleImageView = circle_image_view_other_user_profile
 
         if (!user.photo.isEmpty()){
-            Picasso.get().load(userPhoto).into(circleImageView)
+            Picasso.get().load(user.photo).into(circle_image_view_other_user_profile)
+            Log.d(TAG, "Photo is not empty \n" +
+                    "${user.photo}")
         } else {
             circleImageView.setImageResource(R.drawable.profile_avatar_placeholder_large)
+            Log.d(TAG,"user photo empty \n" +
+                    "${user.photo}")
         }
 
 
