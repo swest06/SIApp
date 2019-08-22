0d369df21ac08cbb17c42f9063c9be539494488e
unknown <swest06@dcs.bbk.ac.uk>
2019-08-22 16:41:46 +0100

    update inbox

--- app/src/main/java/com/project/siapp/InboxActivity.kt
+++ app/src/main/java/com/project/siapp/InboxActivity.kt
@@ -2,11 +2,9 @@ package com.project.siapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
@@ -18,17 +16,23 @@ class InboxActivity : AppCompatActivity() {
     val adapter = GroupAdapter<ViewHolder>()
     val messagesMap = HashMap<String, Message>()
 
+    val TAG = "Inbox Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inbox)
         supportActionBar?.title = "Inbox"
 
-        //set recycler view adapter
-        recycler_view_inbox.adapter = adapter
+
 
         listenForMessages()
+
+        //set recycler view adapter
+        recycler_view_inbox.adapter = adapter
     }
     private fun refreshRecycler(){
+        Log.d(TAG, "Inside refreshRecycler")
+
         adapter.clear()
         messagesMap.values.forEach{
             adapter.add((InboxRow(it)))
@@ -36,19 +40,24 @@ class InboxActivity : AppCompatActivity() {
     }
 
     private fun listenForMessages() {
-
-        val ref = FirebaseDatabase.getInstance().getReference("/latest-messages/$userId")
+        Log.d(TAG, "Inside listenForMessages")
+        val ref = FirebaseDatabase.getInstance().getReference("/inbox/$userId")
 
         //listen for changes in child nodes
         ref.addChildEventListener(object: ChildEventListener{
 
+
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                Log.d(TAG, "Inside onChildAdded")
+
                 val message = p0.getValue(Message::class.java) ?: return
                 messagesMap[p0.key!!] = message
                 refreshRecycler()
 
             }
             override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                Log.d(TAG, "Inside onChildChanged")
+
                 val message = p0.getValue(Message::class.java) ?: return
                 messagesMap[p0.key!!] = message
                 refreshRecycler()
@@ -75,6 +84,29 @@ class InboxActivity : AppCompatActivity() {
 
         override fun bind(viewHolder: ViewHolder, position: Int) {
             viewHolder.itemView.latest_message_textview_inbox_row.text = message.text
+
+            //Get correct id for chat recipient
+            val recipientId: String
+            if  (message.fromId == FirebaseAuth.getInstance().uid){
+                recipientId = message.toId
+            } else {
+                recipientId = message.fromId
+            }
+
+            //get recipient name from database reference
+            val recipientRef = FirebaseDatabase.getInstance().getReference("/users/$recipientId")
+            recipientRef.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    val user = p0.getValue(User::class.java)
+                    viewHolder.itemView.username_textview_inbox_row.text = user?.name
+                }
+            })
+
+
         }
 
     }
